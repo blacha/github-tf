cac97bddef178cd058ac6160c292d9036be8ae65
Blayne Chard <blayne@chard.com>
2023-11-14 21:34:17 +1300

    feat: start configuring repos with cdktf

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,88 @@
+import { DataGithubUser } from '@cdktf/provider-github/lib/data-github-user';
 import { GithubProvider } from '@cdktf/provider-github/lib/provider';
 import { Repository, RepositoryConfig } from '@cdktf/provider-github/lib/repository';
-import { App, S3Backend, TerraformStack } from 'cdktf';
+import { RepositoryEnvironment } from '@cdktf/provider-github/lib/repository-environment';
+import { App, S3Backend, TerraformOutput, TerraformStack } from 'cdktf';
 import { Construct } from 'constructs';
 
-const DefaultSettings: Partial<RepositoryConfig> = {
+const FlowSquashMerge: Partial<RepositoryConfig> = {
   allowMergeCommit: false,
   allowRebaseMerge: false,
+
+  allowSquashMerge: true,
+  squashMergeCommitMessage: 'PR_BODY',
+  squashMergeCommitTitle: 'PR_TITLE',
+
+  deleteBranchOnMerge: true,
 };
+
+const DefaultFeatures: Partial<RepositoryConfig> = {
+  hasProjects: false,
+  hasWiki: false,
+  hasDiscussions: false,
+  hasIssues: true,
+};
+
+function getOrThrow(key: string): string {
+  const val = process.env[key];
+  if (val == null || val.trim() === '') throw new Error('Missing $' + key);
+  return val;
+}
+
+export interface SquashMergeFlowOptions {
+  environments?: ('prod' | 'preprod' | 'nonprod')[];
+  description?: string;
+}
+
+export class SquashMergeFlow {
+  repository: Repository;
+  id: string;
+
+  constructor(scope: Construct, id: string, cfg?: SquashMergeFlowOptions) {
+    this.id = id;
+
+    this.repository = new Repository(scope, id, {
+      ...DefaultFeatures,
+      ...FlowSquashMerge,
+      name: id,
+      description: cfg?.description,
+      visibility: 'public',
+    });
+
+    cfg?.environments?.forEach((env) => {
+      new RepositoryEnvironment(scope, `${id}-env-${env}`, {
+        environment: env,
+        repository: this.repository.name,
+      });
+    });
+  }
+}
 
 class GithubConfig extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // Connect to github, reads GITHUB_TOKEN from env
-    new GithubProvider(this, 'GitHub', {
-      owner: 'blacha',
-    });
+    // Connect to github, reads GitHubCLI/GITHUB_TOKEN from env
+    new GithubProvider(this, 'GitHub', { owner: 'blacha' });
 
     new S3Backend(this, {
-      bucket: process.env['BUCKET_NAME'] ?? '',
+      bucket: getOrThrow('BUCKET_NAME'),
       key: 'github-tf.tfstate',
       region: 'us-east-1',
     });
 
-    const repo = new Repository(this, 'github-tf', {
-      name: 'github-tf',
-      visibility: 'private',
+    new SquashMergeFlow(this, 'github-tf', { description: 'Configure github using CDKTF', environments: ['prod'] });
+    new SquashMergeFlow(this, 'release-test', { environments: ['prod'] });
 
-      ...DefaultSettings,
+    new SquashMergeFlow(this, 'chunkd', { description: '"Chunked file reading abstraction for S3, HTTP and files' });
+    new SquashMergeFlow(this, 'cogeotiff', {
+      description: 'High performance cloud optimized geotiff (COG) reader',
     });
+    new SquashMergeFlow(this, 'binparse', { description: 'Typed binary parsing for typescript' });
+    new SquashMergeFlow(this, 'pretty-json-log', { description: 'Pretty print json log files' });
 
-    repo.allowMergeCommit = false;
+    const currentUser = new DataGithubUser(this, 'current', { username: '' });
+    new TerraformOutput(this, 'current-user', { value: currentUser.name });
   }
 }
 
